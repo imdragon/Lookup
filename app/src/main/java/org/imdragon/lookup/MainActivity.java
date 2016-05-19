932aed538bacc5ce2ea1b983c4bb9126618cc68c
imdragon <i.m.dragon@gmail.com>
2016-05-18 20:46:47 -0700

    Initial commit

---
+++ app/src/main/java/org/imdragon/lookup/MainActivity.java
@@ -0,0 +1,22 @@
+package org.imdragon.lookup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.cardsList);
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+    }
+}
